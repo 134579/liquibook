0e7f173c7e24435301c2eed1e52231d35f67a7a7
Huang Kai <huangkai@work>
2025-07-09 15:55:38 +0800

    Refactor project structure: add CMake configuration files, remove legacy scripts, and update .gitignore

---
+++ cmake/boost.cmake
@@ -0,0 +1,6 @@
+CPMAddPackage(
+  NAME Boost
+  URL https://github.com/boostorg/boost/releases/download/boost-1.87.0/boost-1.87.0-cmake.tar.xz
+  OPTIONS "BUILD_SHARED_LIBS OFF" "BOOST_SKIP_INSTALL_RULES ON" "CMAKE_POSITION_INDEPENDENT_CODE ON" "BOOST_ENABLE_CMAKE ON"
+  "BOOST_INCLUDE_LIBRARIES test"
+)
