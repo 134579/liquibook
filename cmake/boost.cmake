747679bb93a05c34c7e0e953bdc54d2a43f3edfc
Huang Kai <huangkai@work>
2025-08-05 09:53:08 +0800

    Add CMake configuration for depth_feed_publisher and mt_order_entry examples; update BOOST_INCLUDE_LIBRARIES

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -1,6 +1,7 @@
+set(BOOST_INCLUDE_LIBRARIES test integer utility smart_ptr thread)
 CPMAddPackage(
   NAME Boost
   URL https://github.com/boostorg/boost/releases/download/boost-1.87.0/boost-1.87.0-cmake.tar.xz
   OPTIONS "BUILD_SHARED_LIBS OFF" "BOOST_SKIP_INSTALL_RULES ON" "CMAKE_POSITION_INDEPENDENT_CODE ON" "BOOST_ENABLE_CMAKE ON"
-  "BOOST_INCLUDE_LIBRARIES test"
+  # "BOOST_INCLUDE_LIBRARIES test integer utility smart_ptr thread"
 )
